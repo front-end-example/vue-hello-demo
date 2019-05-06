792262e73fb5aa49d6bae99fac976eea63d0d983
Dong <donly@magicalboy.com>
2019-05-06 10:54:12 +0800

    add some lifecycle funs log

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ var app2 = new Vue({
   el: '#app-2',
   data: {
     message: '页面加载于 ' + new Date().toLocaleString()
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate');
+  },
+  created: function() {
+    console.log('message is: ' + this.message);
+  },
+  beforeMount: function() {
+    console.log('beforeMount');
+  },
+  mounted: function() {
+    console.log('mounted');
   }
 });
 var app3 = new Vue({
